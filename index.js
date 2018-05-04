726d81a1bf42cb498aa564c42595e65f18be5f04
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-05-04 01:57:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-let driverId = 0
 let store = {drivers: [], passenger: []}
+let driverId = 0
 
 class Driver {
   constructor(name) {
     this.id = ++driverId
     this.name = name
+    
     store.drivers.push(this)
+  }
+}
+
+let passengerId = 0
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+    
+    store.passengers.push(this)
   }
 }
